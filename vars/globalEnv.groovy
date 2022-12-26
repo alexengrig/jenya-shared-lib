22f7fd0dbb5dcb11b2ba7ce0260c173f39babc4c
Grig Alex <alexengrigdev@gmail.com>
2022-12-26 12:51:09 +0300

    Create globalEnv.groovy

---
+++ vars/globalEnv.groovy
@@ -0,0 +1,8 @@
+#!/usr/bin/env groovy
+
+import dev.alexengrig.sample.jenyasharedlib.GlobalEnv
+
+@SuppressWarnings('GrMethodMayBeStatic')
+void set(String key, String value) {
+    GlobalEnv.createGlobalEnv(key, value)
+}
